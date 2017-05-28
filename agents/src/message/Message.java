f8a71490095edfc3d74e9d34e21a688ec847938d
jlarque <javier_templario@hotmail.com>
2017-05-28 10:54:35 +0200

    Reestructuración

--- agents/src/message/Message.java
+++ agents/src/message/Message.java
@@ -1,27 +1,24 @@
 package message;
 
+import model.InternalStatusAgent;
+
 import java.io.Serializable;
 
 /**
  * Created by jlarque on 26/05/2017.
  */
 public class Message implements Serializable {
-    private Status status;
+    private InternalStatusAgent status;
 
-    public Message (Status status) {
+    public Message (InternalStatusAgent status) {
         setStatus(status);
-	System.out.println(status);
     }
-    public Status getStatus() {
+    public InternalStatusAgent getStatus() {
         return status;
     }
 
-    public void setStatus(Status status) {
+    public void setStatus(InternalStatusAgent status) {
         this.status = status;
     }
 
-    public enum Status {
-        ON,
-        OFF;
-    }
 }
---
+++ agents/src/model/InternalStatusAgent.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * Created by Javi on 28/05/2017.
+ */
+public class InternalStatusAgent {
+
+    private AgentType agentType;
+
+    public InternalStatusAgent(AgentType agentType) {
+        this.agentType = agentType;
+    }
+
+    public enum AgentType {
+        LED,
+        PIR;
+    }
+
+}
