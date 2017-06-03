9e76eeda92771e923dd79c02240f2f60100c78ff
jlarque <javier_templario@hotmail.com>
2017-06-03 20:00:14 +0200

    -

--- agents/src/agents/DatabaseAgent.java
+++ agents/src/agents/DatabaseAgent.java
@@ -1,15 +1,8 @@
 package agents;
 
-import com.pi4j.io.gpio.RaspiPin;
-import model.AgentType;
-import model.PowerStatus;
 import model.behavior.DatabaseBehavior;
-import model.behavior.LedBehavior;
 import model.message.Message;
 import utils.RegisterUtils;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by victoria on 3/06/17.
