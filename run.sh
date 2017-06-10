b59ec63ccc34dd27fc20b2fe36fcbba370651bbc
jlarque <javier_templario@hotmail.com>
2017-06-10 18:16:13 +0200

    -

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export CLASSPATH=lib/jade.jar:lib/pi4j-core.jar:classes/:lib/com.mysql.jdbc_5.1.5.jar
 
-java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4000,suspend=n  jade.Boot -agents "Led:agents.Led;Pir:agents.Pir;Db:agents.DatabaseAgent;Fan:agents.Fan;Temperature:agents.Temperature"
+java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4000,suspend=n  jade.Boot -agents "Led:agents.Led;Pir:agents.Pir;Db:agents.DatabaseAgent;Fan:agents.Fan;Temperature:agents.Temperature;Camera:agents.Camera"
   
